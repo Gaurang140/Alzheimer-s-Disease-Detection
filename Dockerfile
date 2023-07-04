d1d66e6d8071352c289faaf6f0ac37bc13c65ffa
Gaurang <gaurannggirimeghanathi@gmail.com>
2023-07-04 04:49:54 +0200

    final ui and changes2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.8-slim-buster
+FROM python:3.8.15-slim-buster
+
 
 RUN apt update -y && apt install awscli -y   
 WORKDIR /app
--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
+#with open("README.md", "r", encoding="utf-8") as f:
+    #long_description = f.read()
 
 __version__ = "0.0.0"
 
@@ -18,7 +17,7 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="App to detect Alzheimer disease",
-    long_description=long_description,
+    #long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
